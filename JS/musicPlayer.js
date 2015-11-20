a6118908d7a2fe6198cdcaa574b2aec70e36c567
joshuamcshane <joshuamcshane@icloud.com>
2015-11-19 20:10:22 -0500

    Merged everything

---
+++ JS/musicPlayer.js
@@ -0,0 +1,77 @@
+var audio = new Audio("../audio/JumpmanRe.wav"), playbtn, mutebtn, seekslider, volumeslider, seeking=false, seekto, curtimetext, durtimetext;
+
+
+
+function initAudioPlayer(){
+
+	audio.loop = true;
+
+	// Set object references
+	playbtn = document.getElementById("playpausebtn");
+	mutebtn = document.getElementById("mutebtn");
+	seekslider = document.getElementById("seekslider");
+	volumeslider = document.getElementById("volumeslider");
+	curtimetext = document.getElementById("curtimetext");
+	durtimetext = document.getElementById("durtimetext");
+	// Add Event Handling
+	playbtn.addEventListener("click",playPause);
+	mutebtn.addEventListener("click", mute);
+	seekslider.addEventListener("mousedown", function(event){ seeking=true; seek(event); });
+	seekslider.addEventListener("mousemove", function(event){ seek(event); });
+	seekslider.addEventListener("mouseup",function(){ seeking=false; });
+	volumeslider.addEventListener("mousemove", setvolume);
+	audio.addEventListener("timeupdate", function(){ seektimeupdate(); });
+	// Functions
+	function playPause(){
+		if(audio.paused){
+		    audio.play();
+		    playbtn.style.background = "url(../images/pause.png) no-repeat";
+	    } else {
+		    audio.pause();
+		    playbtn.style.background = "url(../images/rsz_1play.png) no-repeat";
+	    }
+	}
+	function mute(){
+		if(audio.muted){
+		    audio.muted = false;
+		    mutebtn.style.background = "url(../images/speaker.png) no-repeat";
+	    } else {
+		    audio.muted = true;
+		    mutebtn.style.background = "url(../images/mute.png) no-repeat";
+	    }
+	}
+	function seek(event){
+	    if(seeking){
+		    seekslider.value = event.clientX - seekslider.offsetLeft;
+	        seekto = audio.duration * (seekslider.value / 100);
+	        audio.currentTime = seekto;
+	    }
+    }
+	function setvolume(){
+	    audio.volume = volumeslider.value / 100;
+		if (audio.volume >= .5){
+		mutebtn.style.background = "url(../images/speaker.png) no-repeat";
+		}
+		if (audio.volume < .5){
+		mutebtn.style.background = "url(../images/lowspeaker.png) no-repeat";
+			if (audio.volume < .1){
+				mutebtn.style.background = "url(../images/mute.png) no-repeat";
+			}
+		}
+	}
+	function seektimeupdate(){
+		var nt = audio.currentTime * (100 / audio.duration);
+		seekslider.value = nt;
+		var curmins = Math.floor(audio.currentTime / 60);
+	    var cursecs = Math.floor(audio.currentTime - curmins * 60);
+	    var durmins = Math.floor(audio.duration / 60);
+	    var dursecs = Math.floor(audio.duration - durmins * 60);
+		if(cursecs < 10){ cursecs = "0"+cursecs; }
+	    if(dursecs < 10){ dursecs = "0"+dursecs; }
+	    if(curmins < 10){ curmins = "0"+curmins; }
+	    if(durmins < 10){ durmins = "0"+durmins; }
+		curtimetext.innerHTML = curmins+":"+cursecs;
+	    durtimetext.innerHTML = durmins+":"+dursecs;
+	}
+}
+window.addEventListener("load", initAudioPlayer);
